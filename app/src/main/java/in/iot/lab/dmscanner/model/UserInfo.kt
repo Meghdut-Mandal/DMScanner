b8b8112ba47e4b67102fdc55a93d405a501bf67d
Meghdut-Mandal <meghdut.windows@gmail.com>
2021-05-09 23:52:03 +0530

    added siginout

--- app/src/main/java/in/iot/lab/dmscanner/model/UserInfo.kt
+++ app/src/main/java/in/iot/lab/dmscanner/model/UserInfo.kt
@@ -1,9 +1,17 @@
 package `in`.iot.lab.dmscanner.model
 
 import com.chibatching.kotpref.KotprefModel
+import com.chibatching.kotpref.bulk
 
 object UserInfo : KotprefModel() {
     var email by stringPref("NONE")
     var name by stringPref("NONE")
     var profile by stringPref("NONE")
+    fun logOut() {
+        this.bulk {
+            email = "NONE"
+            name = "NONE"
+            profile = "NONE"
+        }
+    }
 }
--- app/src/main/java/in/iot/lab/dmscanner/ui/ProfileActivity.kt
+++ app/src/main/java/in/iot/lab/dmscanner/ui/ProfileActivity.kt
@@ -3,11 +3,18 @@ package `in`.iot.lab.dmscanner.ui
 import `in`.iot.lab.dmscanner.R
 import `in`.iot.lab.dmscanner.databinding.ActivityProfileBinding
 import `in`.iot.lab.dmscanner.model.UserInfo
+import `in`.iot.lab.dmscanner.scanner.startActivityX
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.annotation.NonNull
 import coil.load
 import com.firebase.ui.auth.data.model.User
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.ktx.app
 
 class ProfileActivity : AppCompatActivity() {
     lateinit var binding: ActivityProfileBinding
@@ -25,6 +32,19 @@ class ProfileActivity : AppCompatActivity() {
         }
         binding.userEmail.text=UserInfo.email
         binding.userName.text=UserInfo.name
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+        val client = GoogleSignIn.getClient(this, gso)
+
+        binding.logoutButton.setOnClickListener {
+            client.signOut().addOnCompleteListener {
+                FirebaseAuth.getInstance().signOut()
+                UserInfo.logOut()
+                startActivityX(LogInActivity::class.java)
+                finishAffinity()
+            }
+        }
     }
 
 
