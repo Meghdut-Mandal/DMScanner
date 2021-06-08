2435b32d5659bcf64ccee8885d0b94b3744d72da
vivan14 <vivanprakash14@gmail.com>
2021-06-09 00:28:53 +0530

    Histrory added

--- app/src/main/java/in/iot/lab/dmscanner/model/firebaseDataInput.kt
+++ app/src/main/java/in/iot/lab/dmscanner/model/firebaseDataInput.kt
@@ -1,6 +1,7 @@
 package `in`.iot.lab.dmscanner.model
 
 import android.util.Log
+import com.firebase.ui.auth.data.model.User
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -14,27 +15,14 @@ class firebaseDataInput {
         val myRef=database.getReference("registrations")
         Log.d("id",UserInfo.email)
 
-
-
-        firebaseData.email=UserInfo.email
-        firebaseData.name=UserInfo.name
-        firebaseData.isApproved=false
-        firebaseData.productId=qr
-
         myRef.get().addOnSuccessListener{
-            firebaseData.id=((it.childrenCount)+1).toString()
-            Log.d("id",firebaseData.id)
-            myRef.child(firebaseData.id).setValue(firebaseData)
+
+            val firebaseDataobj=firebaseData(UserInfo.email,((it.childrenCount)+1).toInt()
+            ,false,UserInfo.name,qr)
+            myRef.child(firebaseDataobj.id.toString()).setValue(firebaseDataobj)
+
+
         }
-
-//        myRef.addValueEventListener(object : ValueEventListener {
-//            override fun onDataChange(snapshot: DataSnapshot) {
-//
-//            }
-//            override fun onCancelled(error: DatabaseError) {}
-//        })
-
-
 
     }
 
